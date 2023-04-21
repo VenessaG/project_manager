8a415d3453069480ea45647f18a2cd54c6f7981f
Venessa Grota <72497917+VenessaG@users.noreply.github.com>
2023-04-21 13:22:40 +0200

    Create customer.java

---
+++ customer.java
@@ -0,0 +1,6 @@
+public class Customer  extends Poised{
+
+    public Customer(String name, String surname, String telNumber, String email, String address){
+        super(name, surname, telNumber, email, address);
+    }
+}
