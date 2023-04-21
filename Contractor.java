18b0f2ccccfe09d6820b3e97da58657734c83bfb
Venessa Grota <72497917+VenessaG@users.noreply.github.com>
2023-04-21 13:23:22 +0200

    Create Contractor.java

---
+++ Contractor.java
@@ -0,0 +1,6 @@
+public class Contractor extends Person{
+
+    public Contractor (String name, String surname, String telephoneNumber, String email, String address){
+        super(name, surname, telephoneNumber, email, address);
+    }
+}
