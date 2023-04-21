2abc09fa22784922fc99a25a53f24536143d4d68
Venessa Grota <72497917+VenessaG@users.noreply.github.com>
2023-04-21 13:25:17 +0200

    Create Architect.java

---
+++ Architect.java
@@ -0,0 +1,6 @@
+public class Architect extends Person{
+
+    public Architect(String name, String surname, String telNumber, String email, String address) {
+        super(name, surname, telNumber, email, address);
+    }
+}
